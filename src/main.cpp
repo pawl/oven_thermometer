bc83c1a5cea10b6ed5030e7dab39139155d46d9d
Paul Brown <paul90brown@gmail.com>
2020-12-21 18:40:37 -0600

    smooth temperature reading with kalman filter

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "SimpleKalmanFilter.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -24,21 +25,51 @@
 int thermoDO = 19;  // SO
 int thermoCS = 23;
 int thermoCLK = 5;  // SCK
+int thermoVCC = 4;
+// int thermoGND = 2;  // attached to GND
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+float kalmanEst = 0.4, kalmanQ = 0.01;
+SimpleKalmanFilter kalmanFilter(kalmanEst, kalmanEst, kalmanQ);
+
+float filterKalman(float measurement, SimpleKalmanFilter* kalmanFilter)
+{
+  return kalmanFilter->updateEstimate(measurement);
+}
+
+float prevFilteredValue = -1;
+float getFilteredTemperature(MAX6675 thermocouple, float* prevValue, SimpleKalmanFilter* kalmanFilter)
+{
+  // apply kalman filter to temperature data to remove noise
+  // based on: https://github.com/hallgeirl/coffee-roaster/blob/4d4fa73c9831b0ff3f28752c454011ab70410de8/src/roaster/roaster.ino#L112
+  float reading = thermocouple.readFahrenheit();
+
+  if (*prevValue < 0)
+    (*prevValue) = reading;
+
+  float filteredValue = reading;
+  filteredValue = filterKalman(reading, kalmanFilter);
+
+  *prevValue = filteredValue;
+
+  return filteredValue;
+}
+
 void setup() {
   Serial.begin(9600);
+
+  // display is using the 3.3v pin, so need to make another pin output 3.3v
+  pinMode(thermoVCC, OUTPUT); digitalWrite(thermoVCC, HIGH);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
   delay(2000);
-
 }
 
 void loop() {
@@ -47,12 +78,13 @@ void loop() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 1);
-  display.println("Paul's Thermometer");
+  display.println("Thermometer");
 
   display.setTextSize(5);
   display.setTextColor(WHITE);
   display.setCursor(0, 20);
-  display.print((int) thermocouple.readFahrenheit());
+  uint16_t filteredTemp = getFilteredTemperature(thermocouple, &prevFilteredValue, &kalmanFilter);
+  display.print(filteredTemp);
   display.println("F");
 
   display.display();
